b8fa18f30ef57dd4a3ae0713ba51322d22284ff1
kyro38@gmail.com <kyro38@gmail.com@9490c47a-29af-6ade-99e1-741ca723f922>
2011-05-27 08:33:18 +0000

    tutu

--- Jeu.java
+++ Jeu.java
@@ -1,29 +1,29 @@
 class Jeu{
 	Avalam a;
-	
+
 	Joueur J1,J2;
 	Coups c;
 	Historique h;
-	
+
 	boolean finPartie;
 	boolean pause;
-	
-	
+
+
 	public Jeu(Avalam a){
 		this.a = a;
 		J1 = new Joueur(a);
 		J1 = new Joueur(a);
 	}
-	
+
 	void calculerCoup(){
 		c = new Coups(a.f.g.coordToIndince(a.f.g.click),a.f.g.coordToIndince(a.f.g.release));
 	}
-	
+
 	void init(){
 		finPartie=false;
 		pause=false;
-		
+
 		h = new Historique();
 	}
-	
+
 }
