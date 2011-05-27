b904b815d59b2e1ca210ece1b50a21c5bf381b76
kyro38@gmail.com <kyro38@gmail.com@9490c47a-29af-6ade-99e1-741ca723f922>
2011-05-27 11:22:49 +0000

    optionalacon

--- EcouteurDOptions.java
+++ EcouteurDOptions.java
@@ -7,12 +7,75 @@ public class EcouteurDOptions implements ActionListener,
 	Avalam a;
 	Options o;
 
-	EcouteurDOptions (Avalam a) {
+	EcouteurDOptions (Avalam a, Options o) {
 		this.a = a;
-		this.o = a.f.o;
+		this.o = o;
 	}
 
-	public void actionPerformed(ActionEvent e) {}
+	public void actionPerformed(ActionEvent e) {
+		String cmd = e.getActionCommand();
+		System.out.println("Commande " + cmd);
+		if (cmd.equals("ok")) {
+			traiterOk();
+		} else if (cmd.equals("annuler" )) {
+			traiterAnnuler();
+		} else if (cmd.equals("machine1")) {
+			o.n10.setSelected(true);
+			o.n10.setEnabled(true);
+			o.n11.setEnabled(true);
+			o.n12.setEnabled(true);
+			if (o.n10.isSelected())
+				o.typeJ1Tmp = Jeu.BOTLVL1;
+			else if (o.n11.isSelected())
+				o.typeJ1Tmp = Jeu.BOTLVL2;
+			else if (o.n12.isSelected())
+				o.typeJ1Tmp = Jeu.BOTLVL3;
+		} else if (cmd.equals("humain1")) {
+			o.typeJ1Tmp = Jeu.HUMAIN;
+			o.n10.setEnabled(false);
+			o.n11.setEnabled(false);
+			o.n12.setEnabled(false);
+		} else if (cmd.equals("machine2")) {
+			o.n20.setSelected(true);
+			o.n20.setEnabled(true);
+			o.n21.setEnabled(true);
+			o.n22.setEnabled(true);
+			if (o.n20.isSelected())
+				o.typeJ2Tmp = Jeu.BOTLVL1;
+			else if (o.n21.isSelected())
+				o.typeJ2Tmp = Jeu.BOTLVL2;
+			else if (o.n22.isSelected())
+				o.typeJ2Tmp = Jeu.BOTLVL3;
+		} else if (cmd.equals("humain2")) {
+			o.typeJ2Tmp = Jeu.HUMAIN;
+			o.n20.setEnabled(false);
+			o.n21.setEnabled(false);
+			o.n22.setEnabled(false);
+		} else if (cmd.equals("facile1")) {
+			o.typeJ1Tmp = Jeu.BOTLVL1;
+		} else if (cmd.equals("moyen1")) {
+			o.typeJ1Tmp = Jeu.BOTLVL2;
+		} else if (cmd.equals("difficile1")) {
+			o.typeJ1Tmp = Jeu.BOTLVL3;
+		} else if (cmd.equals("facile2")) {
+			o.typeJ2Tmp = Jeu.BOTLVL1;
+			//System.out.println("lvl1 : " + p.typeJ2Tmp);
+		} else if (cmd.equals("moyen2")) {
+			o.typeJ2Tmp = Jeu.BOTLVL2;
+		} else if (cmd.equals("difficile2")) {
+			o.typeJ2Tmp = Jeu.BOTLVL3;
+		} else if (cmd.equals("nomj1")) {
+			// rien
+		} else if (cmd.equals("nomj2")) {
+			// rien
+		} else if (cmd.equals("normal")) {
+			o.modeTmp=true;
+		} else if (cmd.equals("match")) {
+			o.modeTmp=false;
+		} else {
+			System.out.println("Commande '" + cmd + "' n'est pas implémentée");
+		}
+	}
 
 	//// Ecouteur du texte tapé pr les noms
 	public void keyReleased(KeyEvent e) {
@@ -83,10 +146,14 @@ public class EcouteurDOptions implements ActionListener,
 		o.nomJ2Tmp  = o.nomJ2;
 		o.typeJ1Tmp = o.typeJ1;
 		o.typeJ2Tmp = o.typeJ2;
+		a.unpause();
 		o.setVisible(false);
 	}
 
-	public void windowClosed(WindowEvent e) {}
+	public void windowClosed(WindowEvent e) {
+
+	}
+
 	public void windowClosing      (WindowEvent e) {}
 	public void windowOpened       (WindowEvent e) {}
 	public void windowIconified    (WindowEvent e) {}
