975f67d27d2e75ba6b17691a30bd6538249f1e88
clercmargo <clercmargo@9490c47a-29af-6ade-99e1-741ca723f922>
2011-05-30 11:17:21 +0000

    boté

--- trunk/Bot.java
+++ trunk/Bot.java
@@ -6,4 +6,30 @@ public class Bot {
 		this.a = a;
 	}
 	
+	public void choisirBot(){
+	//On choisit pour chaque niveau de l'ordinateur une partie a jouer
+		switch(niveau){
+			case 1 : //bot lvl1
+				partieFacile();
+				break;
+			case 2 : // botlvl2
+				partieMoyen();			
+				break;
+			case 3 : 
+				partieDifficile();
+				break;
+		}
+	}
+	
+	public void partieFacile(){
+	
+	}
+	
+	public void partieMoyen(){
+	
+	}
+	
+	public void partieDifficile(){
+	
+	}
 }
