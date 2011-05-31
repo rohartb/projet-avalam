12794ce93c15709c93044747f35e87a8ebc9bc19
kyro38@gmail.com <kyro38@gmail.com@9490c47a-29af-6ade-99e1-741ca723f922>
2011-05-31 08:21:26 +0000

    tutu

--- Joueur.java
+++ Joueur.java
@@ -1,6 +1,6 @@
 public class Joueur{
 	Avalam a;
-	
+
 	Bot b;
 
 	String nom;
@@ -9,7 +9,21 @@ public class Joueur{
 
 	public Joueur(Avalam a){
 		this.a=a;
+		nom = "tutu";
 	}
-	
+
+
+	public boolean estHumain() {
+		return (type == a.j.HUMAIN );
+	}
+
+	public boolean estRobot() {
+		return (type == a.j.BOTLVL1 || type == a.j.BOTLVL2 ||
+		        type == a.j.BOTLVL3);
+	}
+
+	public boolean estReseau() {
+		return (type == a.j.RESEAU);
+	}
 }
 
