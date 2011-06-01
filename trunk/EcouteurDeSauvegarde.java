9ba80320a781702e7da8a2e7baac7aa5f73f464d
baptiste.venandy <baptiste.venandy@9490c47a-29af-6ade-99e1-741ca723f922>
2011-06-01 11:03:36 +0000

    save

---
+++ trunk/EcouteurDeSauvegarde.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.*;
+
+public class EcouteurDeSauvegarde implements ActionListener {
+    Sauvegarde sauvegarde;
+    
+    public EcouteurDeSauvegarde(Sauvegarde s) {
+        sauvegarde = s;
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("supprimer")) {
+            int value = JOptionPane.showConfirmDialog(sauvegarde.fenetreCharger,
+                                                      "Voulez vous vraiment supprimer \""
+                                                        +sauvegarde.listCharger.getSelectedValue()
+                                                        +"\" ?","Confirmation",
+                                                      JOptionPane.YES_NO_OPTION,
+                                                      JOptionPane.QUESTION_MESSAGE,
+                                                      new ImageIcon("images/question.png"));
+            if(value == JOptionPane.YES_OPTION) {
+                String tmp = (String)sauvegarde.listCharger.getSelectedValue();
+                File fichierTmp = new File(sauvegarde.path+tmp);
+                fichierTmp.delete();
+                sauvegarde.miseAJourListCharger();
+            }
+        } else if (e.getActionCommand().equals("charger")) {
+            System.out.println((String)sauvegarde.listCharger.getSelectedValue());
+            sauvegarde.charger((String)sauvegarde.listCharger.getSelectedValue());
+            sauvegarde.fenetreCharger.dispose();
+        } else if (e.getActionCommand().equals("fermer")) {
+            sauvegarde.fenetreCharger.dispose();
+        }
+    }
+}
