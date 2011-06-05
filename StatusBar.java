4006c4279e6b1f517f7f15b789aa7b31ae74fcd7
alexis.jf.martin <alexis.jf.martin@9490c47a-29af-6ade-99e1-741ca723f922>
2011-06-05 14:30:12 +0000

    le timer ne demmare plus dans le init

--- StatusBar.java
+++ StatusBar.java
@@ -25,7 +25,6 @@ public class StatusBar extends JPanel {
 		timer = new Timer(1000, et);
 		timer.setActionCommand("ticseconde");
 		labelTemps = new JLabel("  " + sdf.format(start));
-		//timer.start();
 
 		this.a = a;
 		PanelTemps = new StatusBarPanel() ;
