81b2d77e0340a167ec596be8331b8f068d3a3e3c
alexis.jf.martin <alexis.jf.martin@9490c47a-29af-6ade-99e1-741ca723f922>
2011-06-05 09:22:14 +0000

    correction bug bouton aide

--- EcouteurDAide.java
+++ EcouteurDAide.java
@@ -3,16 +3,20 @@ import java.awt.event.*;
 public class EcouteurDAide implements MouseListener  {
 	Avalam a;
 	TerrainGraphique g;
+	boolean actif;
 
 
 	public EcouteurDAide (Avalam a, TerrainGraphique g) {
 		this.a = a;
 		this.g = g;
+		actif=true;
 	}
 
 	public void mousePressed(MouseEvent e){
-		a.aide=true;
-		a.unpause();
+		if(actif){
+			a.aide=true;
+			a.unpause();
+		}
 	}
 
 
